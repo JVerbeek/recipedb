d8a1db889dd7e4d2de685263f893275fac9b4334
tostilord <louis.voet@gmail.com>
2020-05-12 13:21:57 +0200

    clean

--- scripts/db-to-tex.py
+++ scripts/db-to-tex.py
@@ -1,11 +1,7 @@
 import os
 
 
-# yo mama is so sfat
-# when I visit her house I am stuck in orbit
-# foo
 def to_tex(filename):
     os.system("context {} --purge".format(filename))
 
 to_tex("/home/janneke/recipedb/TeX-templates/recipe-template")
-# werken de kleurtjes
