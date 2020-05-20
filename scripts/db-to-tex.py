81d3c85b6666d2d77b549d0239bbfb5e60984901
tostilord <louis.voet@gmail.com>
2020-05-20 12:12:18 +0200

    url fix

--- scripts/db-to-tex.py
+++ scripts/db-to-tex.py
@@ -4,4 +4,4 @@ import os
 def to_tex(filename):
     os.system("context {} --purge".format(filename))
 
-to_tex("/home/janneke/recipedb/TeX-templates/recipe-template")
+to_tex("../TeX-templates/recipe-template")
