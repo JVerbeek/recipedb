720f4f7870070a155784b8421caaa1b98fa00374
tostilord <louis.voet@gmail.com>
2020-05-20 12:22:38 +0200

    printer name now a variable

--- scripts/print.py
+++ scripts/print.py
@@ -9,17 +9,19 @@ def argument_handler():
     """
     parser = argparse.ArgumentParser(description="enter description")
     parser.add_argument("file_path", help="path to the file you want to print")
+    parser.add_argument("-printer", "-p", help="name of desired printer",
+                        default = "HP_DeskJet_2600_series_E0A01B_")
     args = parser.parse_args()
     return args
 
 
-def print_file(file_path: str) -> None:
+def print_file(file_path: str, printer: str) -> None:
     """
     Prints a file from the printer in Louis' room.
     :param file_path: path to the file that needs to be printed
     :type file_path: str
     """
-    os.system(f"lpr -P HP_DeskJet_2600_series_E0A01B_ {file_path}")
+    os.system(f"lpr -P {printer} {file_path}")
 
 
 def main(args):
@@ -27,7 +29,7 @@ def main(args):
     Calls print function with a file path.
     :param args: argparse namespace object
     """
-    print_file(os.path.abspath(args.file_path))
+    print_file(os.path.abspath(args.file_path), args.printer)
 
 
 if __name__ == "__main__":
