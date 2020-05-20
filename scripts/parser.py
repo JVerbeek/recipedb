8eb89d0417fec0b66cf6f609232c3f265909c00f
tostilord <louis.voet@gmail.com>
2020-05-20 13:50:30 +0200

    get_recipe update

--- scripts/parser.py
+++ scripts/parser.py
@@ -30,7 +30,7 @@ def user_input():
 
 def get_recipe_list():
     # recipe_list = ["kaasuitosti", "tosti_fake"]
-    recipe_list = [item.rstrip('.xml') for item in os.listdir(os.abspath(recepten))]
+    recipe_list = [item.rstrip('.xml') for item in os.listdir(os.path.abspath(recepten))]
     return recipe_list
 
 
